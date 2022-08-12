88fe5919cbba1314db8119c7ac5c233c480733ad
Bruno Brasil <70863338+brunobf09@users.noreply.github.com>
2022-08-12 16:56:56 -0300

    commit

---
+++ BetRobber.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from betscrapy import scrapy
+import statsmodels.api as sm
+import warnings
+import streamlit as st
+from time import sleep
+warnings.filterwarnings("ignore")
+
+st.image("https://img.freepik.com/vetores-gratis/ilustracao-dos-desenhos-animados-do-roubo-a-banco-no-cofre-forte-dois-ladroes-roubando-ouro-dinheiro_1441-1924.jpg?w=1380&t=st=1660331317~exp=1660331917~hmac=29cdf42b71f66252ee88f8d3e1bbb4968667d9a086fdf733c00c039159bd6b9e")
+st.title('**Bet Robber**')
+
+league = st.text_input("Fill in the Bet page link")
+
+if len(league)>0:
+    df = scrapy([league])
+
+    df.Odd_Betfair = df.Odd_Betfair.astype('float')
+
+    #Selecionando arquivo e aba
+    bf = pd.read_excel('Banca BETFAIR.xlsx','ODD_ROBBER')
+
+    #Modelo 1
+    bf_ = bf[['BETFAIR','ODD_INICIAL_BK']].dropna(axis=0)
+    bf_ = bf_[(bf_.BETFAIR >0) & (bf_.BETFAIR <1.95)]
+    X = bf_.BETFAIR
+    y = bf_.ODD_INICIAL_BK
+    X_sm = sm.add_constant(X)
+    modelo1 = sm.OLS(y, X_sm).fit()
+
+    #Modelo 2
+    bf_ = bf[['BETFAIR','ODD_INICIAL_LY']].dropna(axis=0)
+    bf_ = bf_[(bf_.BETFAIR >2) & (bf_.BETFAIR <3.7)]
+    X = bf_.BETFAIR
+    y = bf_.ODD_INICIAL_LY
+    X_sm = sm.add_constant(X)
+    modelo2 = sm.OLS(y, X_sm).fit()
+
+    #separando dataframes para modelos
+    lay = df[df.Odd_Betfair<=2]
+    back = df[df.Odd_Betfair>2]
+
+    #Regressão lay
+    X = lay.Odd_Betfair
+    X_sm = sm.add_constant(X)
+    df_lay = modelo1.get_prediction(X_sm).summary_frame()
+
+    #Regressão back
+    X = back.Odd_Betfair
+    X_sm = sm.add_constant(X)
+    df_back = modelo2.get_prediction(X_sm).summary_frame()
+
+    reg = pd.concat([df_lay,df_back])
+
+    painel = df.join(reg,how='left')
+
+    st.table(painel)
+
+
+
---
+++ betscrapy.py
@@ -0,0 +1,51 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from time import sleep
+import pandas as pd
+
+def scrapy(league):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome('chromedriver', chrome_options=options)
+
+    link = "https://www.betfair.com/sport/football/"
+
+    driver.get(link)
+
+    while (len(driver.find_elements(By.ID,"onetrust-accept-btn-handler"))) < 1:
+        sleep(0.1)
+
+    accept = driver.find_element(By.ID,"onetrust-accept-btn-handler")
+    accept.click()
+
+    teams = []
+    odds_events = []
+    date = []
+
+    for l in league:
+        driver.get(link + l)
+        while (len(driver.find_elements(By.CLASS_NAME, 'ui-runner-price'))) < 1:
+            sleep(0.2)
+        for odds in driver.find_elements(By.CLASS_NAME, 'ui-runner-price'):
+            odds_events.append(odds.text)
+        for team in driver.find_elements(By.CLASS_NAME, 'team-name'):
+            teams.append(team.text)
+        for d in driver.find_elements(By.CLASS_NAME, 'date'):
+            date.append(d.text)
+
+    driver.quit()
+
+    odd = odds_events[2::5]
+    home = teams[::2]
+    away = teams[1::2]
+
+    betfair = {'Date': date,
+               'HomeTeam': home,
+               'AwayTeam': away,
+               'Odd_Betfair': odd}
+
+    betfair_df = pd.DataFrame(betfair)
+
+    return betfair_df
